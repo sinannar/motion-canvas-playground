535cc25556032b5ab5ee3295f204a9d225340560
Sinan Nar <sinan.nar@firstrescue.co.nz>
2023-03-18 22:53:58 +0300

    adding rectangle

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,10 +1,11 @@
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {Circle} from '@motion-canvas/2d/lib/components';
+import {Circle, Rect} from '@motion-canvas/2d/lib/components';
 import {createRef} from '@motion-canvas/core/lib/utils';
 import {all} from '@motion-canvas/core/lib/flow';
 
 export default makeScene2D(function* (view) {
   const myCircle = createRef<Circle>();
+  const myRect = createRef<Rect>();
 
   view.add(
     <Circle
@@ -21,4 +22,23 @@ export default makeScene2D(function* (view) {
     myCircle().position.x(300, 1).to(-300, 1),
     myCircle().fill('#e6a700', 1).to('#e13238', 1),
   );
+
+  view.removeChildren();
+
+  view.add(
+    <Rect
+      ref={myRect}
+      // try changing these properties:
+      x={-300}
+      width={140}
+      height={140}
+      fill="#e13238"
+    />,
+  );
+
+  yield* all(
+    myRect().position.x(300, 1).to(-300, 1),
+    myRect().fill('#e6a700', 1).to('#e13238', 1),
+  );
+
 });
